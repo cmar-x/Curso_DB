4cfb9030c663c40eb3fa4f74facb7a795d34d1f5
Carlos Anazco <carlos.anazco@gmail.com>
2015-08-28 01:36:14 -0500

    mas Archivos con sentencias SQL

---
+++ Sch2.sql
@@ -0,0 +1,8 @@
+INSERT INTO publishers (name, country)
+	values ('Platzi', 'USA');
+
+INSERT INTO publishers (publisher_id, name, country)
+	values (10, 'Santillana', 'MX');
+
+INSERT INTO publishers (name, country)
+	values ('MIT', 'USA');
---
+++ Sch3.sql
@@ -0,0 +1,7 @@
+INSERT INTO publishers(publisher_id, name, country)
+values (1, 'OReilly', 'USA'),
+(2, 'Santillana', 'Mexico'),
+(3, 'MIT Edu', 'USA'),
+(4, 'UTPC', 'Colombia'),
+(5, 'Platzi', 'USA');
+
